78f24ebbec0437cf39bda015607ec8bf2fa6e85c
JunedConnect <Junedwork@hotmail.com>
2026-06-17 16:19:38 +0100

    feat: update ECS configuration to enable container insights and logging

--- terraform/deployment/modules/ecs/main.tf
+++ terraform/deployment/modules/ecs/main.tf
@@ -1,6 +1,25 @@
 resource "aws_ecs_cluster" "this" {
   name = "${var.environment}-ecs-cluster"
 
+  setting {
+    name  = "containerInsights"
+    value = "enhanced"
+  }
+
+  configuration {
+    execute_command_configuration {
+      logging = "OVERRIDE"
+      log_configuration {
+        cloud_watch_encryption_enabled = true
+        cloud_watch_log_group_name = "/ecs/${var.environment}-execute-command-logs"
+      }
+    }
+  }
+
+}
+
+resource "aws_cloudwatch_log_group" "execute_command_logs" {
+  name = "/ecs/${var.environment}-execute-command-logs"
 }
 
 resource "aws_security_group" "ecs" {
